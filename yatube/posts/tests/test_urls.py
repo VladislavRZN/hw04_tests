7875db08ed3fb0b98180652af5658b74ea8fa130
Владислав Буганов <vladislav-buganov@yandex.ru>
2023-03-29 12:04:15 +0300

    Отправка на ревью. С учётом всех требований ревью что обсуждались в пачке. Сделал для экономии времени пока был в больнице. Исправил flake8

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -40,7 +40,7 @@ class PostURLTests(TestCase):
         self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.author)
-    
+
     def test_guest_urls(self):
         # Проверяем общедоступные страницы
         urls_names = {
